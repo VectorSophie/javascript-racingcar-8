b3a9c9a9a96f3a08db62526228128c1c0b882b1a
VectorSophie <jay7math@gmail.com>
2025-10-22 00:49:19 +0900

    feat(App): skeleton initialization

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Console } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async run() {
+    try{
+      const inputNames = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
+      const inputTryCount = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
+    } catch(error) {
+      Console.print(`[ERROR] ${error.message}`);
+    }
+  }
 }
 
 export default App;
