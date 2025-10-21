94db02da578d8717cdffff26016cf8b3e3cc742e
VectorSophie <jay7math@gmail.com>
2025-10-22 02:30:10 +0900

    refactor(App): refactor redunant variable

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ class App {
   async run() {
     try{
       const inputNames = await Console.readLineAsync('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n');
-      const names = inputNames.split(',')
-      const carNames = names.map((name) => name.trim());
+      const carNames = inputNames.split(',').map((name) => name.trim());
       validateCarNames(carNames);
 
       const inputTryCount = await Console.readLineAsync('시도할 횟수는 몇 회인가요?\n');
